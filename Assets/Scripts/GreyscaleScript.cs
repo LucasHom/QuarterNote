ea754599fef530c10b3c4ace68ce3cf13e1c0d52
TreeWonTwoToFor <jonathan-github@shuman.org>
2025-11-09 15:24:25 -0800

    Created a greyscale shader for the game camera object

---
+++ Assets/Scripts/GreyscaleScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyscaleScript : MonoBehaviour
+{
+    public Shader shader;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = new Material(shader);
+    }
+
+    void Update()
+    {
+        SetGreyscalePercentage(1.0f);
+    }
+
+    void SetGreyscalePercentage(float percentage)
+    {
+        material.SetFloat("_Blend", percentage);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
